a2ade980964adbad9218bb10a3766f6592b1b57e
Guguzz00 <augustoguglielmi@outlook.com>
2019-07-31 17:30:55 +0800

    W

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  #get "restaurants", to: "restaurants#index"
+  #get "restaurants/new", to: "restaurants#new"
+  #post "restaurants", to: "restaurants#create"
+  #get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  #get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  #post "restaurants/:restaurant_id/reviews", to: "reviews#create"
+
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
